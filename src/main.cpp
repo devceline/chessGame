82736abe6c6d18b715d18c0a899594f62002cf0d
Shaheen Sarafa <shaheensarafa17@gmail.com>
2020-12-21 11:49:31 +0200

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
+#include <GL/glew.h>
+#include "GLFW/glfw3.h"
 #include <iostream>
 
 
 int main(void){
-    std::cout << "Hello World!" << std::endl;
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(500, 500, "Testing OpenGL", 0, 0);
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+
+    if(glewInit() != GLEW_OK){
+        std::cout << "Something went wrong!" << std::endl;
+        return 123;
+    }
+
+
     return 0;
 }
